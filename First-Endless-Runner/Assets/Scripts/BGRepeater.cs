be732d81e9b063a40ecf5bb1bea19fdbfce761f8
PNadiadhara <43886212+PNadiadhara@users.noreply.github.com>
2020-04-06 03:34:27 -0400

    background created and variables set

---
+++ First-Endless-Runner/Assets/Scripts/BGRepeater.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGRepeater : MonoBehaviour
+{
+    public float speed;
+
+    public float xStart;
+    public float xend;
+
+
+    private void Update()
+    {
+        transform.Translate(Vector2.left * speed * Time.deltaTime);
+
+        if(transform.position.x <= xend)
+        {
+            Vector2 pos = new Vector2(xStart, transform.position.y);
+            transform.position = pos;
+        }
+    }
+}
